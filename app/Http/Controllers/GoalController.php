9c79071373561ae46f6255c9c10e1aeb2561bfb8
{橋本 <{c9586d5c@ymobile.ne.jp}>
2022-10-28 21:54:49 +0900

    goal_CRUD

--- app/Http/Controllers/GoalController.php
+++ app/Http/Controllers/GoalController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Goal;
 use Illuminate\Http\Request;
-use lluminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Auth;
 
 class GoalController extends Controller
 {
