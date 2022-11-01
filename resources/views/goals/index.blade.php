d5983f6ebb50a957aec94c06f07fd6c3a2bf478e
{橋本 <{c9586d5c@ymobile.ne.jp}>
2022-11-01 21:53:50 +0900

    '10 create_crud'

--- resources/views/goals/index.blade.php
+++ resources/views/goals/index.blade.php
@@ -26,11 +26,11 @@
          <div class="row row-cols-1 row row-cols-md-2 row-cols-lg-3 g-4">                         
              @foreach ($goals as $goal) 
              
-                 <!-- 目標の編集用モーダル -->
-                 @include('modals.edit_goal') 
- 
-                 <!-- 目標の削除用モーダル -->
-                 @include('modals.delete_goal') 
+                <!-- 目標の編集用モーダル -->
+                @include('modals.edit_goal') 
+
+                <!-- 目標の削除用モーダル -->
+                @include('modals.delete_goal') 
                  
                 <!-- ToDoの追加用モーダル -->
                 @include('modals.add_todo')  
@@ -51,6 +51,52 @@
                                  </div>
                              </div>
                          </div>
+
+                        @foreach ($goal->todos()->orderBy('done', 'asc')->get() as $todo) 
+                            <!-- ToDoの編集用モーダル -->
+                            @include('modals.edit_todo')
+
+                            <!-- ToDoの削除用モーダル -->
+                            @include('modals.delete_todo')
+
+                            <div class="card mx-2 mb-2">
+                                <div class="card-body">
+                                    <div class="d-flex justify-content-between align-items-center mb-2">
+                                        <h5 class="card-title ms-1 mb-0">
+                                             @if ($todo->done)
+                                                 <s>{{ $todo->content }}</s>
+                                             @else
+                                                 {{ $todo->content }}
+                                             @endif    
+                                        </h5>
+                                        <div class="dropdown">
+                                            <a href="#" class="dropdown-toggle px-1 fs-5 fw-bold link-dark text-decoration-none menu-icon" id="dropdownTodoMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">︙</a>
+                                            <ul class="dropdown-menu dropdown-menu-end text-center" aria-labelledby="dropdownTodoMenuLink">                                                                                                                                                                                       
+                                                <li>
+                                                    <form action="{{ route('goals.todos.update', [$goal, $todo]) }}" method="post">
+                                                        @csrf
+                                                        @method('patch')
+                                                        <input type="hidden" name="content" value="{{ $todo->content }}">
+                                                        @if ($todo->done)  
+                                                            <input type="hidden" name="done" value="false">
+                                                            <button type="submit" class="dropdown-item btn btn-link">未完了</button>
+                                                        @else
+                                                            <input type="hidden" name="done" value="true">
+                                                            <button type="submit" class="dropdown-item btn btn-link">完了</button> 
+                                                        @endif
+                                                    </form>                                                       
+                                                </li>        
+                                                <li><a href="#" class="dropdown-item" data-bs-toggle="modal" data-bs-target="#editTodoModal{{ $todo->id }}">編集</a></li>                                                  
+                                                <div class="dropdown-divider"></div>
+                                                <li><a href="#" class="dropdown-item" data-bs-toggle="modal" data-bs-target="#deleteTodoModal{{ $todo->id }}">削除</a></li>  
+                                            </ul>
+                                        </div>
+                                    </div>   
+                                    <h6 class="card-subtitle ms-1 mb-1 text-muted">{{ $todo->created_at }}</h6>                                                               
+                                </div>                                
+                            </div>
+                        @endforeach
+
                      </div>                           
                  </div>
              @endforeach                       
